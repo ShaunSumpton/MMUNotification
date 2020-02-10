7024be3ba288d1e0d2ad79db784c1425c07b4384
Shaun Sumpton <s.sumpton@agnortheast.com>
2020-02-10 10:04:57 +0000

    Change from Interop to Net.Mail

--- MMUNotification/Program.cs
+++ MMUNotification/Program.cs
@@ -1,17 +1,7 @@
 ﻿using Microsoft.Office.Interop.Excel;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-using Microsoft.Office.Interop.Outlook;
-using System.Data.SqlClient;
-using System.Data;
-using System.Collections;
-using DataTable = System.Data.DataTable;
-using System.Data.OleDb;
+using System.Net.Mail;
 
 namespace MMUNotification
 {
@@ -42,22 +32,13 @@ namespace MMUNotification
             var UK = application.WorksheetFunction.CountIf(uknot, "UK"); // count uk sends
             var NONUK = application.WorksheetFunction.CountIf(uknot, "Non-UK"); // count nonuk sends
 
-            Microsoft.Office.Interop.Outlook.Application app = new Microsoft.Office.Interop.Outlook.Application(); // create outlook instance
-            MailItem mailItem = app.CreateItem(OlItemType.olMailItem); // create mail item
-
-
-
-
-            mailItem.Subject = "MMU Data Notification " + DateTime.Now.ToString("dd/MM/yyyy");                                                    // set up email with to,subject, body etc
-            mailItem.To = "s.sumpton@agnortheast.com; S.kent@agnortheast.com ; r.richardson@agnortheast.com";
-
-
-            mailItem.Importance = OlImportance.olImportanceHigh;
-            mailItem.Display(false); // dont display mail item before sending
-            _ = mailItem.HTMLBody;
-            var body = "MMU Offer Guide Quantities <br /> <br />" + "Number of UK: " + UK + "<br />" + "Number of Non-UK: " + NONUK + "<br />" + "--------------------------------------";
-            mailItem.HTMLBody = body; //+ signature;
-           mailItem.Send(); // senmail confirming data count
+            MailMessage message = new MailMessage("s.sumpton@agnortheast.com", "S.Sumpton@agnortheast.com; s.kent@agnortheast.com; a.granger@agnortheast.com; r.Richardson@agnortheast.com",
+                "MMU Data Notification " + DateTime.Now.ToString("dd/MM/yyyy"),
+                "MMU Offer Guide Quantities <br /> <br />" + "Number of UK: " + UK + "<br />" + "Number of Non-UK: " + NONUK + "<br />" + "--------------------------------------");
+            message.IsBodyHtml = true;
+            SmtpClient client = new SmtpClient("6.1.1.143");
+            client.Send(message);          
+          
 
         }
     }
